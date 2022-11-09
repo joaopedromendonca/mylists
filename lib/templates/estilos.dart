b9200dc00d3016cd4798d0f0446fe8dadac09fe8
joaopedromendonca <joaoooh@gmail.com>
2022-11-09 17:54:02 -0300

    Mudança no background  Modificação do background de widget para função que retorna um container

--- lib/templates/estilos.dart
+++ lib/templates/estilos.dart
@@ -2,26 +2,22 @@
 
 import 'package:flutter/material.dart';
 
-class BackGround extends StatelessWidget {
-  const BackGround({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: double.infinity,
-      width: double.infinity,
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          colors: [
-            Color.fromARGB(255, 127, 174, 233),
-            Color.fromARGB(255, 78, 143, 218),
-            Color.fromARGB(255, 45, 120, 212),
-            Color.fromARGB(255, 5, 72, 148),
-          ],
-        ),
+Widget backGround(Widget child) {
+  return Container(
+    height: double.infinity,
+    width: double.infinity,
+    decoration: BoxDecoration(
+      gradient: LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: [
+          Color.fromARGB(255, 127, 174, 233),
+          Color.fromARGB(255, 78, 143, 218),
+          Color.fromARGB(255, 45, 120, 212),
+          Color.fromARGB(255, 5, 72, 148),
+        ],
       ),
-    );
-  }
+    ),
+    child: child,
+  );
 }
